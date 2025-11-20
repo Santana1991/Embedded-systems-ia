625069cb077ec20303c523885a59ccb0be5e3056
santana <salvadorassc@gmail>
2025-11-19 20:30:51 -0600

    feat:display_7_improved

--- displey_de_7_segmentos_improved.X/displaey_improved.c
+++ displey_de_7_segmentos_improved.X/displaey_improved.c
@@ -1,21 +1,22 @@
 /*
- * File:   DISPLAY.c
+ * File:   displaey_improved.c
  * Author: SANTA
  *
- * Created on 19 de noviembre de 2025, 07:26 PM
+ * Created on 19 de noviembre de 2025, 08:14 PM
  */
+
+
 #include <xc.h>
 #define _XTAL_FREQ 4000000
-
 #include "PIC16F877ACONFIG.H"
-// Tabla 
-const unsigned char tabla[10] = {0x3F, 0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F};
+
+const unsigned char tabla[10] = {0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F};
 
 void main(void){
-    // Configuración pines
-    TRISB = 0x00;       // PORTB como salida (display)
+    
+    TRISB = 0x00;       // triseb como salida (display)
     PORTB = 0x00;
-    TRISDbits.TRISD0 = 1; // RD0 entrada para pulsador
+    TRISDbits.TRISD0 = 1; // rd0 entrada para pulsador
 
     unsigned char contador = 0;
     PORTB = tabla[contador];
@@ -28,7 +29,7 @@ void main(void){
                 if(contador > 9) contador = 0;
                 PORTB = tabla[contador];
                 while(PORTDbits.RD0 == 0); // Espera a soltar
-                __delay_ms(20); // Antirrebote al soltar
+                __delay_ms(20); 
             }
         }
     }
---
+++ displey_de_7_segmentos_improved.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=displey_de_7_segmentos_improved.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/displey_de_7_segmentos_improved.X.production.hex
